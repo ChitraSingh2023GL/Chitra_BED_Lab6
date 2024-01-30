e32fbfdf257311336e461cd51a8adcedd6aa5540
ChitraSingh2023GL <146869240+ChitraSingh2023GL@users.noreply.github.com>
2024-01-30 23:36:01 +0530

    Add files via upload

---
+++ src/test/java/com/greatLearning/LibrayManagemrnt/EventManagementApplicationTests.java
@@ -0,0 +1,13 @@
+package com.greatLearning.LibrayManagemrnt;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LibrayManagemrntApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
